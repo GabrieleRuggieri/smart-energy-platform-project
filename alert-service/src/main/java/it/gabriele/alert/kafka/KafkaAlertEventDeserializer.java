c05e2ac088379a81c56199e1dde894507db75aba
GabrieleRuggieri <gabri20022002@gmail.com>
2025-05-28 21:52:13 +0200

    deserializer for alerts model

---
+++ alert-service/src/main/java/it/gabriele/alert/kafka/KafkaAlertEventDeserializer.java
@@ -0,0 +1,40 @@
+package it.gabriele.alert.kafka;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import it.gabriele.alert.model.BaseAlertEvent;
+import it.gabriele.alert.model.BrazilAlertEvent;
+import it.gabriele.alert.model.TestAlertEvent;
+import org.apache.kafka.common.serialization.Deserializer;
+
+import java.util.Map;
+
+public class KafkaAlertEventDeserializer implements Deserializer<BaseAlertEvent> {
+
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    public void configure(Map<String, ?> configs, boolean isKey) {}
+
+    @Override
+    public BaseAlertEvent deserialize(String topic, byte[] data) {
+        try {
+            JsonNode tree = mapper.readTree(data);
+            String source = tree.get("source").asText();
+
+            if ("BrazilModel".equalsIgnoreCase(source)) {
+                return mapper.treeToValue(tree, BrazilAlertEvent.class);
+            } else if ("TestModel".equalsIgnoreCase(source)) {
+                return mapper.treeToValue(tree, TestAlertEvent.class);
+            } else {
+                throw new IllegalArgumentException("Unknown source type: " + source);
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to deserialize alert event", e);
+        }
+    }
+
+    @Override
+    public void close() {}
+}
