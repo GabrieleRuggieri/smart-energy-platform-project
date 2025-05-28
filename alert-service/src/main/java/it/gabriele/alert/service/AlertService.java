f439e027951e71d121631c7a045c80911b344a60
GabrieleRuggieri <gabri20022002@gmail.com>
2025-05-28 21:53:34 +0200

    alert handling

---
+++ alert-service/src/main/java/it/gabriele/alert/service/AlertService.java
@@ -0,0 +1,21 @@
+package it.gabriele.alert.service;
+
+import io.quarkus.logging.Log;
+import it.gabriele.alert.model.BaseAlertEvent;
+import it.gabriele.alert.model.BrazilAlertEvent;
+import it.gabriele.alert.model.TestAlertEvent;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class AlertService {
+
+    public void handle(BaseAlertEvent event) {
+        if (event instanceof BrazilAlertEvent b) {
+            Log.infof("Handled Brazil alert: consumption = {%s}", b.getHourlyEnergyConsumption());
+        } else if (event instanceof TestAlertEvent t) {
+            Log.infof("Handled Test alert: temp = {%s}, building = {%s}", t.getTemperature(), t.getBuildingType());
+        } else {
+            Log.warnf("Unhandled alert type: {}", event.getClass().getSimpleName());
+        }
+    }
+}
